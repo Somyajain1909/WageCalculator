949a8548ef057ef19a7af93f32e1ef3062416719
Somyajain1909 <202052339@iiitv.ac.in>
2023-05-31 15:17:50 +0530

    done

---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import App2 from './App2'
+import RowState from './context/RowState';
+
+
+const App = () => {
+  return (
+    <div>
+        <RowState> <App2/></RowState>
+     
+    </div>
+  )
+}
+
+export default App
